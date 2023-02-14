dd19d0b923f097d1c06fd18ee4f5c44afdb8cdc0
Gabriel <mouradgabriel9@gmail.com>
2023-02-14 08:51:25 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Test</title>
+</head>
+<body>
+    <header>
+	
+    </header>
+
+    <section>
+      <?php
+
+	$name = 'John Doe';
+
+	?>
+
+	<h1>Bonjo<?php echo $name; ?>uer </h1>
+    </section>
+
+    <footer>
+       
+    </footer>
+</body>
+</html>
+
