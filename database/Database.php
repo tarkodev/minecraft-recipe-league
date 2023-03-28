a70db9da8478fd32bb7c1d4f9fc4b947a04a9e34
Tarkod <mouradgabriel9@gmail.com>
2023-03-28 08:52:44 +0200

    Final final final commit?

--- database/Database.php
+++ database/Database.php
@@ -1,30 +1,13 @@
 <?php
+include_once "database/pdo.php";
+
 
 class Database {
 
     private $pdo;
 
     public function __construct(){
-        header("Content-Security-Policy: default-src 'self'");
-
-        $database_file = "database/database.sqlite";
-
-        $this->pdo = new PDO("sqlite:" . $database_file);
-        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $this->pdo->query('CREATE TABLE IF NOT EXISTS posts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title VARCHAR(255) NOT NULL,
-            content TEXT NOT NULL
-        );');
-
-        $create_table_query = "CREATE TABLE IF NOT EXISTS users (
-            id     INTEGER NOT NULL PRIMARY KEY,
-            total  INTEGER DEFAULT 0
-        )";
-
-        $this->pdo->exec($create_table_query);
+        $this->pdo = getPdo();
     }
 
     function generateUserAndGetId() {
---
+++ database/pdo.php
@@ -0,0 +1,26 @@
+<?php
+
+function getPdo() {
+    header("Content-Security-Policy: default-src 'self'");
+
+    $database_file = "database/database.sqlite";
+
+    $pdo = new PDO("sqlite:" . $database_file);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->query('CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title VARCHAR(255) NOT NULL,
+            content TEXT NOT NULL
+        );');
+
+    $create_table_query = "CREATE TABLE IF NOT EXISTS users (
+            id     INTEGER NOT NULL PRIMARY KEY,
+            total  INTEGER DEFAULT 0
+        )";
+
+    $pdo->exec($create_table_query);
+
+    return $pdo;
+}
