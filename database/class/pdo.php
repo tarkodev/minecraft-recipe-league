4aa2205d18126f370bbc2adce06f254a6b74b30f
Tarkod <mouradgabriel9@gmail.com>
2023-04-19 14:48:41 +0200

    fix fac computer

--- database/class/pdo.php
+++ database/class/pdo.php
@@ -9,12 +9,6 @@ function getPdo() {
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $pdo->query('CREATE TABLE IF NOT EXISTS posts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title VARCHAR(255) NOT NULL,
-            content TEXT NOT NULL
-        );');
-
     $create_table_query = "CREATE TABLE IF NOT EXISTS users (
             id     INTEGER NOT NULL PRIMARY KEY,
             total  INTEGER DEFAULT 0
